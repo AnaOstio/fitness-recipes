a7fa21e85b9597ea233df49456d263be8dd8a98d
Carlos <57149778+C4rlos99@users.noreply.github.com>
2023-06-14 09:52:01 +0200

    Script refactor and added timeForPreparation field

--- scripts/script.py
+++ scripts/script.py
@@ -1,56 +1,37 @@
-import csv
 import os
 import pandas as pd
 import random
 import numpy as np
 
-# Obtiene la ruta absoluta del directorio actual
-#directorio_actual = os.path.dirname(os.path.abspath(__file__))
+# Get and read the dataSet file
+currentDir = os.path.dirname(os.path.abspath(__file__))
+filePath = os.path.join(currentDir, 'food.csv')
 
-# Concatena el nombre del archivo CSV a la ruta del directorio actual
-# ruta_archivo = os.path.join(directorio_actual, 'food.csv')
+dataFrame = pd.read_csv(filePath)
 
-# Abre el archivo CSV en modo lectura
-# with open(ruta_archivo, 'r') as archivo_csv:
-#    lector_csv = csv.reader(archivo_csv)
-#    for linea in lector_csv:
-#       print(linea)
-
-
-# Ruta al archivo CSV
-directorio_actual = os.path.dirname(os.path.abspath(__file__))
-ruta_archivo = os.path.join(directorio_actual, 'food.csv')
-
-# Lee el archivo CSV usando pandas
-dataframe = pd.read_csv(ruta_archivo)
-
-# Elimina la columna específica del dataframe
+# Drop the unnecessary fields
 nombre_columna = 'Cleaned_Ingredients'
-dataframe = dataframe.drop(nombre_columna, axis=1)
+dataFrame = dataFrame.drop(nombre_columna, axis=1)
 
-# Añadimos la informacion del tipo de comida
-tipoComida = ["Breakfast", "Lunch", "Dinner", "Dessert", "Mid-morning meal"]
-tipoRandom = [random.choice(tipoComida) for _ in range(len(dataframe))]
+# Add missing fields
+typeOfMeal = ["Breakfast", "Lunch", "Dinner", "Dessert", "Mid-morning meal"]
+randomTypeOfMeal = [random.choice(typeOfMeal) for _ in range(len(dataFrame))]
 
-dataframe["TypeMeal"] = tipoRandom
-
-# Añadimos la informacion del Rating
 rating = np.arange(0, 5.5, 0.5).tolist()
-typeRating = [random.choice(rating) for _ in range(len(dataframe))]
+randomRating = [random.choice(rating) for _ in range(len(dataFrame))]
 
-dataframe["Rating"] = typeRating
+macroLabels = ["Carbohydrates", "Greases", "Protein", "Fiber"]
+randomMacroPercentage = np.round(np.random.dirichlet(np.ones(4), len(dataFrame))*100, decimals=2)
+macroPerentagesSet = [{macroLabels[j]:randomMacroPercentage[i][j] for j in range(len(macroLabels))} for i in range(len(randomMacroPercentage))]
 
-# Macro Nutrients %
-macro = ["carbohydrates", "greases", "protein", "fiber"]
+minutes = np.arange(10, 180, 2).tolist()
+timeOfPreparation = [str(random.choice(minutes)) + " minutes" for _ in range(len(dataFrame))]
 
-randomPercentage = np.round(np.random.dirichlet(np.ones(4), len(dataframe))*100, decimals=2)
+dataFrame["TypeOfMeal"] = randomTypeOfMeal
+dataFrame["Rating"] = randomRating
+dataFrame["macronutrientsPercentages"] = macroPerentagesSet
+dataFrame["timeOfPReparation"] = timeOfPreparation
 
-macroSet = [{macro[j]:randomPercentage[i][j] for j in range(len(macro))} for i in range(len(randomPercentage))]
-
-dataframe["macronutrientsPercentages"] = macroSet
-
-# Guarda el dataframe actualizado en un nuevo archivo CSV
-ruta_nuevo_archivo = os.path.join(directorio_actual, 'food6.csv')
-dataframe.to_csv(ruta_nuevo_archivo, index=False)
-
-print("Columna eliminada y archivo CSV actualizado correctamente.")
+# Save the cleaned dataSet file
+newFilePath = os.path.join(currentDir, 'foodFilled.csv')
+dataFrame.to_csv(newFilePath, index=False)
