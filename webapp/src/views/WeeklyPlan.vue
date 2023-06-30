8c406ef93b4fc9fe5621cc95b50c78f53f9c042e
Ana Fernández Ostio <anafdezostio01@gmail.com>
2023-06-30 12:57:18 +0200

    feat(WeekPlan): change html to vue with components

--- webapp/src/views/WeeklyPlan.vue
+++ webapp/src/views/WeeklyPlan.vue
@@ -1,12 +1,46 @@
 <template>
+  <div>
+    <h1>Week Plan</h1>
+    <div class="week">
+        <recipe-list-week-component></recipe-list-week-component>
+        <recipe-list-week-component></recipe-list-week-component>
+        <recipe-list-week-component></recipe-list-week-component>
+        <recipe-list-week-component></recipe-list-week-component>
+        <recipe-list-week-component></recipe-list-week-component>
+        <recipe-list-week-component></recipe-list-week-component>
+        <recipe-list-week-component></recipe-list-week-component>
+    </div>
+  </div>
+
 
 </template>
 
-<script setup>
+<script>
+import RecipeListWeekComponent from "@/components/RecipeListWeekComponent.vue";
+export default {
+  name: "WeeklyPlan",
+  components: {
+    RecipeListWeekComponent,
+    },
+}
 
 </script>
 
 
-<style scoped>
+<style>
+  .week {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    text-align: center;
+  }
+  @media screen and (max-width: 800px) {
+    .week {
+      overflow-x: scroll;
+      background-color: aqua;
+      width: 700%;
+    }
+  }
 
 </style>
